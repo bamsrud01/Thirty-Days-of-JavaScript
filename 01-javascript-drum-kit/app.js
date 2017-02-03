8cc6ca44ebde4a678562eb5d510e5ea87728b5c0
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-02 22:32:47 -0600

    Added .gitignore, audio files

--- 01-javascript-drum-kit/app.js
+++ 01-javascript-drum-kit/app.js
@@ -1,9 +1,10 @@
-// Listen for keydown event
+// Function to listen for keydown event, and play associated audio file
 window.addEventListener('keydown', function(e) {
   //  Check to see if pressed key's keycode is linked to a data-key attribute
     // If not, audio is null
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  // console.log(audio);
+  // console.log(audio);  //  Logs the audio element
   if (!audio) return;
+  // Play audio file
   audio.play();
 });
