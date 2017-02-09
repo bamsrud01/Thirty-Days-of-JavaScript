ee0e76988f64ec20e00e1ba829294a8fc4d54402
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-09 17:33:12 -0600

    Implemented basic line drawing

--- 08-Canvas/app.js
+++ 08-Canvas/app.js
@@ -12,6 +12,30 @@ canvas.height = window.innerHeight;
 // Context settings
   // Stroke style - color of line
   // Line join & line cap - Is the line round or square when it meets another, or ends?
-ctx.strokestyle = '#BADA55';
+ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+
+// Controls of when and where a line is drawn
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+  if (!isDrawing) return;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY); //  Start point
+  ctx.lineTo(e.offsetX, e.offsetY); //  End point
+  ctx.stroke();
+  // Update position, using ES2015 feature
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+// Event listeners to control drawing of line
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
