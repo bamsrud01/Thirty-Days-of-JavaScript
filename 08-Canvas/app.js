f6bab43d599c3941c58128b1f3cf98ffb0472c0c
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-09 17:56:42 -0600

    Line color and width changes as drawn

--- 08-Canvas/app.js
+++ 08-Canvas/app.js
@@ -12,23 +12,41 @@ canvas.height = window.innerHeight;
 // Context settings
   // Stroke style - color of line
   // Line join & line cap - Is the line round or square when it meets another, or ends?
-ctx.strokeStyle = '#BADA55';
+ctx.strokeStyle = 'black';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
+ctx.lineWidth = 1;
 
 // Controls of when and where a line is drawn
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 
+// Controls of line style
+let hue = 0;
+let direction = true;
+
 function draw(e) {
   if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   ctx.moveTo(lastX, lastY); //  Start point
   ctx.lineTo(e.offsetX, e.offsetY); //  End point
   ctx.stroke();
   // Update position, using ES2015 feature
   [lastX, lastY] = [e.offsetX, e.offsetY];
+  // Increment color
+  if (hue >= 360) hue = 0;
+  hue ++;
+  // Adjust line width
+  if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 }
 
 // Event listeners to control drawing of line
