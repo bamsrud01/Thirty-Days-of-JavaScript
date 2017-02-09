adaa32c046c0edff6da4fff3ac3dc3ccd3b96a54
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-08 20:30:00 -0600

    Completed all tasks

--- 07-Array-Cardio-2/app.js
+++ 07-Array-Cardio-2/app.js
@@ -34,9 +34,28 @@ console.log({anotherIsAdult});
   // Syntax implicitly returns arrow function
 const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
 console.log({allAdults});
+
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const comment = comments.find(function(comment) {
+  if (comment.id == 823423) {
+    return true;
+  }
+});
+console.log({comment});
+
+// Convert to arrow function with implicit return
+const arrowComment = comments.find( comment => (comment.id == 823423));
+console.log({arrowComment});
+
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const index = comments.findIndex(comment => comment.id == 823423);
+console.log({index});
+
+const newComments = [
+  ...comments.slice(0, index),
+  ...comments.slice(index + 1)
+];
