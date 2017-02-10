9be709dbd88b8eb9d0e45fdf0587eca320cc83ff
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-10 13:21:49 -0600

    Completed functionality, updated README.md

--- 09-Dev-Tools/README.md
+++ 09-Dev-Tools/README.md
@@ -1,10 +1,11 @@
 # Dev Tool Tricks
 
-
+This repo just practices the use of multiple console methods.  They can be seen in the console
 
 ## Features
 
-
+- String and style interpolation
+- Multiple console methods: .warn, .error, .info, .assert, .clear, .dir, .group
 
 ## Author
 
--- 09-Dev-Tools/app.js
+++ 09-Dev-Tools/app.js
@@ -5,14 +5,34 @@ function makeGreen() {
   p.style.fontSize = '50px';
 }
 // Regular
+console.log('hello');
 // Interpolated
+console.log('hello, I am an %s string!', 'interpolated');
 // Styled
+console.log('%c I am styled text', 'font-size:50px; background:red;');
 // warning!
+console.warn('Warning!');
 // Error :|
+console.error('ERROR!');
 // Info
+console.info('Info or fact here');
 // Testing
+console.assert(1 == 2, 'That is wrong!');  //  If first arg is false, will send error message
 // clearing
+console.clear();
 // Viewing DOM Elements
+console.dir(p);  //  opens a dropdown of things in that element
+console.clear();
 // Grouping together
+dogs.forEach(dog => {
+  console.group(`${dog.name}`);
+  console.log(`This is ${dog.name}.`);
+  console.log(`${dog.name} is ${dog.age} years old.`);
+  console.log(`That's ${dog.age * 7} years old in dog years.`);
+  console.groupEnd(`${dog.name}`);
+});
+
 // counting
+console.count('Counts the number of times using this argument.');
 // timing
+console.time('Fetching data...');
