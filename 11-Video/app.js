60318a82ea5e6ae420175fe8ddb1896cf76df0d0
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-12 23:30:29 -0600

    Play and pause video, update button icon

--- 11-Video/app.js
+++ 11-Video/app.js
@@ -10,4 +10,21 @@ const ranges = player.querySelectorAll('.player__slider');
 
 /*  Build Functions  */
 
+function togglePlay() {
+  // Can build a ternary operation
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
 /*  Build Event Listeners  */
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
+
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
