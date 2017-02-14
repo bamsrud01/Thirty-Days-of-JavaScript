7253b429c75853f95517da12245ba8f5911056d2
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-14 16:29:03 -0600

    Completed functionality

--- 13-Slide/app.js
+++ 13-Slide/app.js
@@ -1,3 +1,4 @@
+//  Provided function, ensuring functions run every 20 milliseconds rather than every millisecond
 function debounce(func, wait = 20, immediate = true) {
   var timeout;
   return function() {
@@ -12,3 +13,26 @@ function debounce(func, wait = 20, immediate = true) {
     if (callNow) func.apply(context, args);
   };
 }
+
+//  Select elements
+const sliderImages = document.querySelectorAll('.slide-in');
+
+//  Function to run when user scrolls
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    //  Halfway through image and bottom of image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    //  Determine if image is half shown
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  });
+}
+
+//  Event listeners
+window.addEventListener('scroll', debounce(checkSlide));
