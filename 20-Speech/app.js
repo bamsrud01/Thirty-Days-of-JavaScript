ad8437ab4412453c00a3860023fea15595a257db
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-21 22:12:08 -0600

    Speech recognition gathers appropriate information

--- 20-Speech/app.js
+++ 20-Speech/app.js
@@ -1,3 +1,32 @@
 //  Speech recognition is global in the browser, but some browsers have a different name.
 //  This keeps the format consistent through the application
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+//  Ensure that speech is populating while speaking, instead of waiting for speech to finish
+recognition.interimResults = true;
+
+//  Create new paragraph element
+let p = document.createElement('p');
+
+//  Append new paragraphs to DOM
+const words = document.querySelector('.words');
+words.appendChild(p);
+
+//  Event listeners
+
+//  This listener will run only once.  When speech is finished, it stops
+recognition.addEventListener('result', e => {
+  console.log(e.results);
+  //  Create a string from the results
+  const transcript = Array.from(e.results)
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
+});
+
+//  This event listener will restart the speech recognition event listener
+recognition.addEventListener('end', recognition.start);
+
+//  Start the application
+recognition.start();
