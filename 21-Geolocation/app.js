f1090d88f8c266041c296d3a3fcacdd521348b1e
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-22 22:48:16 -0600

    Completed scripts

--- 21-Geolocation/app.js
+++ 21-Geolocation/app.js
@@ -1,1 +1,11 @@
-// Geolocation
+//  Select elements
+const arrow = document.querySelector('.arrow');
+const speec = document.querySelector('.speed-value');
+
+navigator.geolocation.watchPosition((data) => {
+  speed.textContent = data.coords.speed;
+  arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+}, (err) => {
+  console.err(err);
+  alert('You must enable location to use this application.');
+});
