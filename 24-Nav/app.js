ff498ac6c73af5f79f426a5fc4d2127b477e7796
Barrett Amsrud <barrettamsrud@gmail.com>
2017-02-25 19:00:05 -0600

    Completed funtionality

---
+++ 24-Nav/app.js
@@ -0,0 +1,17 @@
+//  Select elements
+const nav = document.querySelector('#main');
+const topOfNav = nav.offsetTop;
+
+//  Function to determine when the nav should maintain a fixed position
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+//  Event listeners
+window.addEventListener('scroll', fixNav);
